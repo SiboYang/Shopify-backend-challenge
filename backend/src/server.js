3f2ce5116b5eceb68506d440f75cfc2c1c49542b
SiboYang <siboyang419@gmail.com>
2022-01-18 12:14:21 -0500

    for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,12 @@
 import app from "./app.js";
+import path from "path";
+import { fileURLToPath } from "url";
 const PORT = process.env.PORT || 8080;
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../../frontend/build"));
+}
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
