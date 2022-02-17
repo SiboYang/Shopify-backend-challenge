db6a546af34ff1abc5311e4b16e0d030c689a909
SiboYang <siboyang419@gmail.com>
2022-02-16 19:14:21 -0500

    change base url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,4 +1,3 @@
 import axios from 'axios';
-//const instance = axios.create({baseURL: 'https://shopify-sibo.herokuapp.com/'});
-const instance = axios.create({baseURL: 'http://localhost:8080'});
+const instance = axios.create({baseURL: 'https://shopify-sibo.herokuapp.com/'});
 export default instance
